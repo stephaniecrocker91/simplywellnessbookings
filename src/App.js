d25cde005a3632ede044ffd7bef62badfeebeb69
Stephie <stephaniecrocker91@gmail.com>
2023-02-13 22:25:39 +0000

    Create home component and stylesheet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Simply Wellness Bookings</h1>
       <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  return (
+    // <div className={styles.image}>
+    <div>
+      <h1>Your wellness journey starts here...</h1>
+      <h2>Book your first class</h2>
+    </div>
+  )
+}
+
+export default Home
