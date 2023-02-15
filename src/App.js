d7046ca27a4946595ddebef1f8f5c66d41038f8a
Stephie <stephaniecrocker91@gmail.com>
2023-02-15 11:38:16 +0000

    Import container from react dom, and add content there

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import { Container } from 'react-bootstrap';
+// import { Route,Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Simply Wellness Bookings</h1>
+    <div className={styles.App}>
       <NavBar />
-      <Home />
+    <Container className={styles.Main}>
+        <Home />
+    </Container>
+
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 const Home = () => {
   return (
     // <div className={styles.image}>
-    <div>
+    <div className={styles.Home}>
       <h1>Your wellness journey starts here...</h1>
       <h2>Book your first class</h2>
     </div>
